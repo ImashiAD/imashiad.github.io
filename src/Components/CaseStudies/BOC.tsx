ef593aed53d1cc1d7c18666b274a7ff3ac45cc3e
Imashi <imashi.dissanayake@avtra.com>
2025-04-22 11:48:00 +0000

    Add case study implementation

---
+++ src/Components/CaseStudies/BOC.tsx
@@ -0,0 +1,10 @@
+const BOC = () => {
+  return (
+    <>
+      <h1>BOC </h1>
+      <p>poo poo</p>
+    </>
+  );
+};
+
+export default BOC;
--- src/Components/CaseStudy.tsx
+++ src/Components/CaseStudy.tsx
@@ -1,5 +1,11 @@
-const CaseStudy = () => {
-  return <h1></h1>;
+import React from "react";
+import { useParams } from "react-router-dom";
+import BOC from "./CaseStudies/BOC";
+
+const CaseStudy: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  if (id == "1") return <BOC />;
+  return <div>Case study not found.</div>;
 };
 
 export default CaseStudy;
