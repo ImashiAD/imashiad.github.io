b19fdbde235fcaabda6b7237048356a7b1a6e1b7
Naushikha <dev@naushikha.com>
2025-05-19 21:30:55 +0530

    Add pringy

--- src/Components/My3DScene.tsx
+++ src/Components/My3DScene.tsx
@@ -1,12 +1,40 @@
 // BedroomScene.tsx
-import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Environment,
+  OrbitControls,
+  useAnimations,
+  useGLTF,
+} from "@react-three/drei";
 import * as THREE from "three";
 
 function BedroomModel() {
   const gltf = useGLTF("./bedroom.glb"); // Make sure this path is correct
   return <primitive object={gltf.scene} />;
+}
+
+function Pringle() {
+  const group = useRef<THREE.Group>(null);
+  const { scene, animations } = useGLTF("./pringle.glb");
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    const idleAction = actions["Idle"];
+    if (idleAction) {
+      idleAction.setEffectiveTimeScale(0.7);
+      idleAction.reset().play();
+    }
+  }, [actions]);
+
+  return (
+    <primitive
+      ref={group}
+      object={scene}
+      position={[1, 0, 0]}
+      rotation={[0, 1.2, 0]}
+    />
+  );
 }
 
 export default function My3DScene() {
@@ -16,16 +44,20 @@ export default function My3DScene() {
       <directionalLight position={[5, 10, 5]} intensity={1} />
 
       <BedroomModel />
+      <Pringle />
+      <Environment preset="city" />
 
       <OrbitControls
         enablePan={false}
         minDistance={2}
-        maxDistance={4}
+        maxDistance={2.5}
         minPolarAngle={Math.PI / 4} // restrict vertical movement
         maxPolarAngle={Math.PI / 2.5} // restrict vertical movement
         minAzimuthAngle={-Math.PI / 6}
         maxAzimuthAngle={Math.PI / 1.7}
-        target={[0, 0.5, 0]}
+        target={[1, 0.7, 0]}
+        rotateSpeed={0.2}
+        panSpeed={0.2}
       />
     </Canvas>
   );
