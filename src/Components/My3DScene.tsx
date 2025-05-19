fcd2431503caff44d64d8fd1deca34242bcdc1e0
Naushikha <dev@naushikha.com>
2025-05-19 21:40:06 +0530

    Fix initial camera location

--- src/Components/My3DScene.tsx
+++ src/Components/My3DScene.tsx
@@ -4,6 +4,7 @@ import { Canvas } from "@react-three/fiber";
 import {
   Environment,
   OrbitControls,
+  PerspectiveCamera,
   useAnimations,
   useGLTF,
 } from "@react-three/drei";
@@ -42,6 +43,11 @@ export default function My3DScene() {
     <Canvas style={{ width: "100%", height: "100%" }}>
       <ambientLight intensity={0.6} />
       <directionalLight position={[5, 10, 5]} intensity={1} />
+      <PerspectiveCamera
+        makeDefault
+        position={[4, 1, 1]} // your desired starting position
+        fov={75}
+      />
 
       <BedroomModel />
       <Pringle />
